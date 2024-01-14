444f2454ff1da93f52a4263900bd5275c2fb208d
Przemek Dudek <przemod77@gmail.com>
2024-01-14 17:50:35 +0100

    feat: Changed action of new button, added choice of specialization etc

--- force-app/main/default/lwc/appointmentMaker/appointmentMaker.js
+++ force-app/main/default/lwc/appointmentMaker/appointmentMaker.js
@@ -1,16 +1,79 @@
-import { LightningElement, wire, api } from 'lwc';
-import getDoctorsByFacility from '@salesforce/apex/AppointmentController.getDoctorsByFacility';
+import { LightningElement, wire, api, track } from 'lwc';
+
+import getDoctorsBySpecialization from '@salesforce/apex/AppointmentController.getDoctorsBySpecialization';
+import getSpecializations from '@salesforce/apex/AppointmentController.getSpecializations';
 
 export default class AppointmentMaker extends LightningElement {
-    @api recordId;
-    doctors;
+    //@api recordId;
 
-    @wire(getDoctorsByFacility, { facilityId: '$recordId' })
+    @track selectedSpecialization = '';
+    @track doctors = [];
+    @track specializationOptions = [];
+
+    @wire(getSpecializations, {})
+    getSpecializations({ data, error }) {
+        if (data) {
+            this.specializationOptions = data.map(item => {
+                return {
+                    label: item,
+                    value: item
+                };
+            });
+            this.error = undefined;
+            console.log('Specialization Options:', this.specializationOptions);
+        } else if (error) {
+            this.error = error;
+            this.specializationOptions = [];
+            console.error('Error fetching specializations:', error);
+        }
+    }
+
+    get specOptions() {
+        return this.specializationOptions;
+    }
+
+    @wire(getDoctorsBySpecialization, { specialization: '$selectedSpecialization' })
     wiredDoctors({ error, data }) {
         if (data) {
-            this.doctors = data;
+            this.doctors = data.map(doctor => ({
+                ...doctor,
+                buttonLabel: `${doctor.First_Name__c} ${doctor.LastName__c}`
+            }));
+            this.error = undefined;
         } else if (error) {
+            this.error = error;
+            this.doctors = [];
             console.error('Error fetching doctors:', error);
         }
+    }
+
+    handleSpecializationChange(event) {
+        this.selectedSpecialization = event.detail.value;
+        console.log('Selected Specialization:', this.selectedSpecialization);
+    }
+
+    handleMouseOver(event) {
+        const doctorId = event.target.dataset.key;
+        const doctorIndex = this.doctors.findIndex(doc => doc.Id === doctorId);
+        if (doctorIndex !== -1) {
+            this.doctors[doctorIndex].buttonLabel = 'Make Appointment';
+        }
+    }
+
+    handleMouseOut(event) {
+        const doctorId = event.target.dataset.key;
+        const doctorIndex = this.doctors.findIndex(doc => doc.Id === doctorId);
+        if (doctorIndex !== -1) {
+            this.doctors[doctorIndex].buttonLabel = `${this.doctors[doctorIndex].First_Name__c} ${this.doctors[doctorIndex].LastName__c}`;
+        }
+    }
+
+    handleButtonClick() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__component',
+            attributes: {
+                componentName: 'c__yourOtherLWC'
+            }
+        });
     }
 }
